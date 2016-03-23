d3becb817ce6c6339b115de763025dc845ca56f5
Cedric Toncanier <cedric.toncanier@gmail.com>
2016-03-23 09:07:18 -0400

    Add BLBeanStuff

---
+++ iOs/FindBean/FindBean/BLBeanStuff.h
@@ -0,0 +1,13 @@
+//
+//  BLBeanStuff.h
+//  FindBean
+//
+//  Created by Cédric Toncanier on 2016-03-23.
+//  Copyright © 2016 Cédric Toncanier. All rights reserved.
+//
+
+#ifndef BLBeanStuff_h
+#define BLBeanStuff_h
+
+
+#endif /* BLBeanStuff_h */
---
+++ iOs/FindBean/FindBean/BLBeanStuff.m
@@ -0,0 +1,9 @@
+//
+//  BLBeanStuff.m
+//  FindBean
+//
+//  Created by Cédric Toncanier on 2016-03-23.
+//  Copyright © 2016 Cédric Toncanier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
